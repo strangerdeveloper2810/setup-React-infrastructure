50fed9158dd9eb0aa2c3bdefa98efdeb951f2f2f
Trình Nguyễn <nguyenmaster3012@gmail.com>
2024-09-12 00:50:05 +0700

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Header/i);
+  const footerElement = screen.getByText(/Footer/i);
+  expect(headerElement).toBeInTheDocument();
+  expect(footerElement).toBeInTheDocument();
 });
